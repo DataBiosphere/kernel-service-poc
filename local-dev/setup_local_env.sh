a87e4c179ac5235e9f201bfcedd01f93edff4d09
zloery <zloery@gmail.com>
2020-02-28 11:05:51 -0500

    Change skaffold.yaml to use template, so that people won't automatically check their local changes into git.

--- local-dev/setup_local_env.sh
+++ local-dev/setup_local_env.sh
@@ -14,7 +14,7 @@ NAMESPACE=$1
 git clone https://github.com/DataBiosphere/kernel-service-poc-config
 
 # Point skaffold to the new kustomize base
-sed -i '' "s|NAMESPACE|${NAMESPACE}|g" skaffold.yaml
+sed "s|NAMESPACE|${NAMESPACE}|g" skaffold.yaml.template > skaffold.yaml
 
 # Replace kernel-poc-service default config with the local changes
 sed -i '' "s|github.com/databiosphere/kernel-service-poc//config?ref=0.0.0|../../../config|g" kernel-service-poc-config/${NAMESPACE}/kustomization.yaml
