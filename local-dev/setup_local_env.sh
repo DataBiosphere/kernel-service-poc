600a54212dfdb2012b916ff1a99aed27a3084852
zloery <zloery@gmail.com>
2020-03-03 09:16:57 -0500

    Idea for continuous dev workflow (#9)
    * Add skaffold local development scripts
    
    * Add local-dev to .gitignore. Other than the provided template, files in this dir should stay local
    
    * Update local-dev/README
    
    * Remove old comment line
    
    * Update README with skaffold/local dev features
    
    * Change skaffold.yaml to use template, so that people won't automatically check their local changes into git.
    
    * Fix typos and update README

---
+++ local-dev/setup_local_env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Helper script to set up Skaffold for local development.
+# This clones the kernel-service-poc-config git repo,
+# points it to the local version of this repo (rather than a published version),
+# and points skaffold to the -config repo's kustomize.
+# If you need to pull changes to the -config repo, just delete your local kernel-service-poc-config directory and rerun the script.
+
+#Required input
+NAMESPACE=$1
+
+# For other services forking this TEMPLATE repo, change this line to the appropriate config repo. 
+git clone https://github.com/DataBiosphere/kernel-service-poc-config
+
+# Point skaffold to the new kustomize base
+sed "s|NAMESPACE|${NAMESPACE}|g" skaffold.yaml.template > skaffold.yaml
+
+# Replace kernel-poc-service default config with the local changes
+sed -i '' "s|github.com/databiosphere/kernel-service-poc//config?ref=0.0.0|../../../config|g" kernel-service-poc-config/${NAMESPACE}/kustomization.yaml
+
+# That's it! You can now deploy to the k8s cluster by running
+# $ skaffold run
+# Or by using IntelliJ's Cloud Code integration, which will auto-detect the generated skaffold.yaml file.
