fba12ef8718af38b3e9d013b4b8122e34b77564c
zloery <zloery@gmail.com>
2020-04-06 16:07:21 -0400

    Update API definition and swagger-ui file to support in-cluster development with Skaffold

--- src/main/java/bio/terra/TEMPLATE/app/configuration/ApiResourceConfig.java
+++ src/main/java/bio/terra/TEMPLATE/app/configuration/ApiResourceConfig.java
@@ -9,9 +9,8 @@ public class ApiResourceConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry
-        .addResourceHandler("/swagger-webjar/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.24.0/");
+    registry.addResourceHandler("/api/swagger-webjar/**").addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.24.0/");
+    // registry.addResourceHandler("swagger-webjar/**").addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.24.0/");
     registry.addResourceHandler("/api/**").addResourceLocations("classpath:/api/");
   }
 }
